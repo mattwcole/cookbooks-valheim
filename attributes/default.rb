5682b49ade4c90d04a8d6445ddd8b4e10b9d882d
Matt Cole <matt@matt-cole.co.uk>
2021-03-27 13:59:11 +0000

    Run as systemctl service

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['vhserver']['user'] = 'vhserver'
+default['vhserver']['install_dir'] = '/home/vhserver'
+
+default['vhserver']['config']['servername'] = 'Chef Valheim'
+default['vhserver']['config']['serverpassword'] = ''
+default['vhserver']['config']['port'] = '2456'
+default['vhserver']['config']['savedir'] = '$HOME/.config/unity3d/IronGate/Valheim'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,39 +1,67 @@
-user 'vhserver' do
+user = node['vhserver']['user']
+home_dir = "/home/#{user}"
+install_dir = node['vhserver']['install_dir']
+serverfiles_dir = "#{node['vhserver']['install_dir']}/serverfiles"
+vhserver_file = "#{node['vhserver']['install_dir']}/vhserver"
+server_config = node['vhserver']['config']
+
+user user do
+    home home_dir
     manage_home true
 end
 
-remote_file '/home/vhserver/linuxgsm.sh' do
+remote_file "#{install_dir}/linuxgsm.sh" do
     source 'https://linuxgsm.sh'
     mode '0755'
-    owner 'vhserver'
+    owner user
+    action :create_if_missing
 end
 
 execute 'linuxgsm' do
     command './linuxgsm.sh vhserver'
-    cwd '/home/vhserver'
-    user 'vhserver'
+    cwd install_dir
+    user user
     live_stream true
-    not_if { ::File.exist?('/home/vhserver/vhserver') }
+    not_if { ::File.exist?(vhserver_file) }
 end
 
 execute 'vhserver_install_dependencies' do
     command './vhserver auto-install'
-    cwd '/home/vhserver'
+    cwd install_dir
     live_stream true
+    not_if { ::Dir.exist?(serverfiles_dir) }
 end
 
 execute 'vhserver_install' do
     command './vhserver auto-install'
-    cwd '/home/vhserver'
-    user 'vhserver'
-    environment ({'HOME' => '/home/vhserver'})
+    cwd install_dir
+    user user
+    environment ({'HOME' => home_dir})
     live_stream true
+    not_if { ::Dir.exist?(serverfiles_dir) }
 end
 
-execute 'vhserver_start' do
-    command './vhserver start'
-    cwd '/home/vhserver'
-    user 'vhserver'
-    environment ({'HOME' => '/home/vhserver'})
-    live_stream true
+template "#{install_dir}/lgsm/config-lgsm/vhserver/vhserver.cfg" do
+    source 'vhserver.cfg.erb'
+    owner user
+    variables(
+        :servername => server_config['servername'],
+        :serverpassword => server_config['serverpassword'],
+        :port => server_config['port'],
+        :savedir => server_config['savedir']
+    )
+    notifies :restart, 'service[vhserver]', :delayed
+end
+
+template "/etc/systemd/system/vhserver.service" do
+    source 'vhserver.service.erb'
+    variables(
+        :user => user,
+        :install_dir => install_dir
+    )
+    notifies :restart, 'service[vhserver]', :delayed
+end
+
+service 'vhserver' do
+    action [:enable, :start]
 end
